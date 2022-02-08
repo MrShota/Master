bd6776e9d6027c7be41c139aa2ca8e151ec9a005
shota <shot.shavadze@gmail.com>
2022-02-08 16:43:56 +0400

    commit to Master

---
+++ JS/script.js
@@ -0,0 +1,74 @@
+// "use strict"
+
+// console.log("Hello world!");
+// console.log ("Im MrBlack")
+
+// var a = 20
+// // console.log(a)
+
+// var b = 10 
+// // console.log (a, b)
+// // console.log (a + b)
+
+// var c = a + b
+// // console.log(c)
+
+// // console.log(0+true)
+ 
+// // console.log(typeof b)
+
+// // alert("hello world")
+// // prompt()
+// // confir()
+// // prompt ("enter your name: ")
+// // var name = prompt ("enter your name: ")
+// // var age= prompt("enter your age")
+// // console.log(name)
+// // console.log ("you are" + "years old")
+// var x = prompt("enter number 1: ")
+// var y = prompt("enter number 2: ")
+// console.log("x + y =" + x + y)
+// console.log("x - y =" + (x - y))
+// console.log("x / y =" + x / y)
+// console.log("x * y =" + x * y)
+
+
+// var x = +prompt ("enter year of birth")
+// var y = +prompt ("enter desired year")
+// // console.log (y-x)
+// alert ("you will be " + (y-x) + " years old")
+
+// var radius = + prompt("enter radius of circle")
+// var PI= 3.14
+// console.log (PI * radius * radius)
+
+// var C= + prompt ("enter in celsius")
+// var F= 2.66
+// console.log (C * F)
+
+
+
+// var GEL = +prompt ("enter GEL")
+// var USD =2.98 
+// var EURO =3.42
+// console.log ("ოპერაციის შედეგი: USD")
+// console.log ( GEL / USD)
+// console.log ("ოპერაციის შედეგი:EURO ")
+// console.log (GEL / EURO )
+
+
+
+
+/* var date = new Date(); // მიმდინარე თარიღის მიღება
+var time = date.getHours(); // მიმდინარე დრო საათებში
+if(time < 13) // შევადაროთ რიცხვს 13
+    alert('დილა მშვიდობისა!'); // მიმდინარე დრო ნაკლებია 13 საათზე
+else
+    // alert('საღამო მშვიდობისა!'); // მიმდინარე დრო მეტია 13 საათზე */
+
+
+    // var a=5-8;
+    // document.write ("ოპერაციის შედეგი:")
+    // document.write (a)
+    // console.log ("ოპერაციის შედეგი:")
+    // console.log (a)
