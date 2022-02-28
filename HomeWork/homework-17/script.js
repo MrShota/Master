38aa43df5605ced4ecbcc0d7b4b9c166c23784eb
shota <shot.shavadze@gmail.com>
2022-02-28 16:06:45 +0400

    commit onMaster

---
+++ HomeWork/homework-17/script.js
@@ -0,0 +1,24 @@
+var reads = +prompt (`enter readers`)
+var read = document.querySelector(`#read`);
+var rea = document.querySelector(`#rea`);
+var re = document.querySelector(`#re`);
+read.textContent =  reads  +  ` Readers`
+rea.textContent =  reads  +  ` Readers`
+re.textContent =  reads  +  ` Readers`
+
+
+var views = +prompt (`enter views`)
+var view = document.querySelector(`#view`);
+var vie = document.querySelector(`#vie`);
+var vi = document.querySelector(`#vi`);
+view.textContent =  views  +  ` Viewers`
+vie.textContent =  views  +  ` Viewers`
+vi.textContent =  views  +  ` Viewers`
+
+var comments = +prompt (`enter comments`)
+var comment = document.querySelector(`#comment`);
+var commen = document.querySelector(`#commen`);
+var comme = document.querySelector(`#comme`);
+comment.textContent =  comments  +  ` Comments`
+commen.textContent =  comments  +  ` Comment`
+comme.textContent =  comments  +  ` Commen`
